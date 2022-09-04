77f5d4eb48a7fe87b2aec6f9fd2255f1f921d2d1
戴勤 <1046562917@qq.com>
2022-09-04 12:56:08 +0800

    feat: init project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Helloworld from "../components/Helloworld.vue";
 import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
@@ -6,27 +6,26 @@ import Home from "../pages/Home.vue";
 
 //配置路由区域
 const routes = [
-    {
-        path: "/Home",
-        component: Home
-    },    
-    {
-        path: "/Helloworld",
-        component: Helloworld
-    },
-    {
-        path: "/Login",
-        component: Login
-    },
-    {
-        path: "/Register",
-        component: Register
-    },
-]
+  {
+    path: "/Home",
+    component: Home,
+  },
+  {
+    path: "/Helloworld",
+    component: Helloworld,
+  },
+  {
+    path: "/Login",
+    component: Login,
+  },
+  {
+    path: "/Register",
+    component: Register,
+  },
+];
 
 //声明路由器并暴露
 export const router = createRouter({
-    history: createWebHistory(),
-    routes: routes
-})
-
+  history: createWebHistory(),
+  routes: routes,
+});
