e12971ca9a761e52f26a94884e3ded73b641ca12
haungyuLocal <None>
2022-09-10 04:22:26 +0800

    feat: Home init 1#

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import App from './App.vue'
 import { router } from "./router";//引入路由器
 
 
-createApp(App).use(router).mount('#app')
+
+createApp(App).use(router).use(ElementPlus).mount('#app')
