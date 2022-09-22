bf427329625048bf59613e5732b98d39880f4e3f
戴勤 <1046562917@qq.com>
2022-09-23 01:45:41 +0800

    feat: 🐛 layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    proxy:{
+    proxy: {
       //测试代理配置，成功
-      '/api':{
-        target:'http://localhost:3000',   
-        ws: true,     
+      "/api": {
+        target: "http://localhost:3000",
+        ws: true,
         changeOrigin: true,
       },
-      '/static': {
-        target:'http://localhost:8080',   
-        ws: true,     
+      "/static": {
+        target: "http://localhost:8080",
+        ws: true,
         changeOrigin: true,
-      }
-    }
+      },
+    },
   },
   // css: {
   //   preprocessorOptions: {
@@ -27,6 +27,4 @@ export default defineConfig({
   //     }
   //   }
   // }
-})
-
-
+});
